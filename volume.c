9587908d92f62c222626066a584115c5ccb5521b
Louis Rossmann <louis@rossmanngroup.com>
2023-08-27 16:00:40 -0500

    Let's make it work without pressing enter

--- volume.c
+++ volume.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <termios.h>            //termios, TCSANOW, ECHO, ICANON
+#include <unistd.h>     //STDIN_FILENO
 
-main()
-{
-        int c;
-        c = 0;
-        while ((c = getchar()) != EOF)
-        {
-                if (c == 'y')
-                {
-                        int status = system("./volumeup");
-                }
-                if (c == 'n')
-                {
-                        int status = system("./volumedown");
-                }
-        }
+int main(void){
+    static struct termios oldt, newt;
+    /*tcgetattr gets the parameters of the current terminal
+    STDIN_FILENO will tell tcgetattr that it should write the settings
+    of stdin to oldt*/
+    tcgetattr( STDIN_FILENO, &oldt);
+    /*now the settings will be copied*/
+    newt = oldt;
+
+    /*ICANON normally takes care that one line at a time will be processed
+    that means it will return if it sees a "\n" or an EOF or an EOL*/
+    newt.c_lflag &= ~(ICANON);          
+
+    /*Those new settings will be set to STDIN
+    TCSANOW tells tcsetattr to change attributes immediately. */
+    tcsetattr( STDIN_FILENO, TCSANOW, &newt);
+
+
+int c;
+	while ((c = getchar()) != 'e'){
+		if (c == ']'){
+			int status = system("pactl -- set-sink-volume 0 +1%");
+			}
+		if (c == '['){
+			int status = system("pactl -- set-sink-volume 0 -1%");
+			}
+	}
 }
